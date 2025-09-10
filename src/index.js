7414d2542217cfc4ee8ed68b51b9eac706d38a1b
Satish Nikale <satishnikale7@gmail.com>
2025-09-10 10:34:42 +0530

    add owner, admin routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import { PORT } from "./config/config.js";
 import cors from "cors";
 import userRoutes from "./routes/userRoute.js";
+import ownerRoutes from "./routes/ownerRoute.js";
+import adminRoutes from "./routes/adminRoute.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/owner", ownerRoutes);
+app.use("/api/v1/admin", adminRoutes);
 
 async function main() {
   app.listen(PORT);
